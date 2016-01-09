081933e02beeed720a76a567349afc4ac7a08395
Dhiraj Pandgare <dhirajpandagre12@gmail.com>
2016-01-09 19:11:10 +0530

    First Commit

---
+++ broad.js
@@ -0,0 +1,11 @@
+
+
+var change=function()
+{
+
+var b=document.getElementById("ravi");
+var a=document.getElementById("sec1");
+	b.innerHTML="Have fun";
+	b.style.color="white";
+	a.style.backgroundImage="url('ocean.jpg')";	
+};
---
+++ news.js
@@ -0,0 +1,12 @@
+var main = function()
+{
+	alert("Thank you for visiting");
+	btn1=document.getElementById("btn");
+	btn1.innerHTML="hello";
+	nav=document.querySelector('.nav')
+	
+};
+.btn:hover
+{
+	color:red;
+}
